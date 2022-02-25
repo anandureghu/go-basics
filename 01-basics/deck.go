7777b1612b31683c3e29516bd99f28918d0d99e9
anandureghu <anandu.reghu@qburst.com>
2022-02-25 13:13:44 +0530

    Multiple return, Byte Slice, Type conversion, Strings, joining, split, File save, read, error handling, shuffleing, swap, random, seed, time, io, if

--- 01-basics/deck.go
+++ 01-basics/deck.go
@@ -1,11 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
 type deck []string
 
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func (d deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
+
+//Outre functions
 func newDeck() deck {
 	cards := deck{}
 
@@ -20,8 +58,13 @@ func newDeck() deck {
 	return cards
 }
 
-func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading from file\n", err)
+		os.Exit(1)
 	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- 01-basics/main.go
+++ 01-basics/main.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
+	cards.saveToFile("cards")
 
-	cards.print()
+	newCards := newDeckFromFile("cards")
+	fmt.Println(newCards.toString())
+
+	newCards.shuffle()
+
+	newCards.print()
 }
 
 func newCard() string {
